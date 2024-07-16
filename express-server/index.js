db83364db26a3aff4a4d76e3712e62de0845aecb
Rangari <nrangari@webmd.net>
2024-07-16 19:31:28 +0530

    CORS issue resolved by adding headers

--- express-server/index.js
+++ express-server/index.js
@@ -5,6 +5,20 @@ const { validateUrl } = require('./validations/appValidations');
 const app = express();
 const PORT = 3000;
 
+// Handling CORS
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
+    if (req.method === "OPTIONS") {
+      // Handle preflight requests (sent by browsers before making actual requests)
+      res.status(200).end();
+    } else {
+      next();
+    }
+  });
+
 // Route for checking server status
 app.get("/", (req, res) => {
     res.send("Server is Successfully Running, and App is listening on port " + PORT);
